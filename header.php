f14148352ef786e313d6c9e19846410ee20dc62a
Muto Sota <hasu_m.s@icloud.com>
2023-11-09 20:05:10 +0900

    initial commit

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <?php wp_head(); ?>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+
+    <title>卒業展示会2024</title>
+    <meta name="description" content="公立はこだて未来大学情報デザインコースの卒業展示会2024" />
+
+    <link rel="stylesheet" href="style.css">
+</head>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php get_header() ?>
+<header>
+    <h1 class="title">
+        <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/title.svg" alt="title">
+    </h1>
+</header>
+
+<main>
+
+</main>
+
+<?php get_footer() ?>

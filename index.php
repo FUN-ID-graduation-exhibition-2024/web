fb74a658c46761a3ee3f145607ef5c25268ee1c7
Muto Sota <hasu_m.s@icloud.com>
2023-11-16 21:03:52 +0900

    update: 11/15作業分

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php get_header() ?>
+
+<div class="graphic01">
+    <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/grafic01.png" alt="">
+</div>
+
 <header>
     <h1 class="title">
         <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/title.svg" alt="title">
@@ -6,7 +11,11 @@
 </header>
 
 <main>
-
+    <div class="section">
+        <h2 class="subtitle">
+            <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/concept.svg" alt="">
+        </h2>
+    </div>
 </main>
 
 <?php get_footer() ?>

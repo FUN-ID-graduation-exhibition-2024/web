501eda3862837a065bb5269fb13d73ef5b528e95
Muto Sota <hasu_m.s@icloud.com>
2023-11-28 21:06:45 +0900

    update: 11/28作業分

--- footer.php
+++ footer.php
@@ -1,8 +1,11 @@
 <footer>
-
+    <div class="funlogo text__center">
+        <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/funlogo.png" alt="">
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
+<script src="https://kit.fontawesome.com/f025f1da34.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 <div class="graphic01">
     <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/grafic01.png" alt="">
 </div>
+<div class="graphic02">
+    <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/grafic02.png" alt="">
+</div>
+<div class="graphic03">
+    <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/grafic03.png" alt="">
+</div>
 
 <header>
     <h1 class="title">
@@ -15,7 +21,127 @@
         <h2 class="subtitle">
             <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/concept.svg" alt="">
         </h2>
+        <div class="section__content text__center line-height__54">
+            <h3 class="subsubtitle">自己中心デザイン</h3>
+            <p class="section__text">
+                制限された日々が、少しずつだけど自由になってきた。<br>
+                今までやりたかったけどできなかったことが、<br>
+                溜め込んできた好奇心が、<br>
+                大きなエネルギーになっている。<br>
+                <br>
+                殻を「トゲ」で打ち破って、<br>
+                自分たちの見せたいもの、<br>
+                作りたいもの、伝えたいものを、研究にできた。<br>
+                最後の年ぐらい、<br>
+                「自己中心デザイン」ってトガっていい。
+            </p>
+        </div>
     </div>
+
+    <div class="section">
+        <h2 class="subtitle">
+            <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/introduction.svg" alt="">
+        </h2>
+        <div class="section__content text__center">
+            <p class="section__text text__left line-height__48">
+                私たち公立はこだて未来大学情報デザインコースは、<br>
+                情報社会で豊かに生きるためのデザイン理論や表現方法に加え、<br>
+                情報科学や認知科学の最新の理論と実践を学んできました。<br>
+                コロナ禍を過ごした私たちにとって、<br>
+                当たり前が当たり前じゃなくなったことが数多くありました。<br>
+                しかし、ようやく忘れかけていた日々も帰ってきたように感じます。<br>
+                一度、日常が途切れてしまった私たちにしか生み出せないモノがあると思います。<br>
+                やりたいことができなかった4年分の好奇心を、<br>
+                卒業研究として形にした集大成をぜひご覧ください。
+            </p>
+        </div>
+    </div>
+
+    <div class="section">
+        <h2 class="subtitle">
+            <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/information.svg" alt="">
+        </h2>
+        <div class="section__content text__center">
+            <p class="section__text text__left font__20">
+                <span class="text__left font__36">
+                    公立はこだて未来大学情報デザインコース<br>
+                    卒業研究展覧会2024『トゲる』<br>
+                </span>
+                <br>
+                <span class="font__32">
+                    函館展
+                </span>
+                <br>
+                <span class="font__normal">
+                    日時：2/8（木）10:00 〜 17:00<br>
+                    会場：<a href="https://g-sq.jp/" target="_blank">函館コミュニティプラザGスクエア</a>
+                    <br>
+                    　　　(北海道函館市本町24番1号シエスタハコダテ4階)<br>
+                </span>
+                <br>
+                <span class="font__32">
+                    東京展
+                </span>
+                <br>
+                <span class="font__normal">
+                    日時：02/22（木）13:00 〜 21:00、02/23（金・祝）10:00 〜 17:00<br>
+                    会場：<a href="https://www.uchida.co.jp/company/showroom/canvas/tokyo/index.html" target="_blank">株式会社内田洋行 ユビキタス協創広場 CANVAS</a>
+                    <br>
+                    　　　(東京都中央区新川2-4-7 B1階)<br>
+                </span>
+                <br>
+                <span class="font__32">
+                    入場無料・事前予約不要
+                </span>
+            </p>
+        </div>
+    </div>
+
+    <div class="section">
+        <h2 class="subtitle">
+            <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/attention.svg" alt="">
+        </h2>
+        <div class="section__content text__center">
+            <h3 class="subsubtitle">
+                注意事項<br>
+                (コロナに関する諸注意・来場者へのお願い)
+            </h3>
+            <p class="section__text font__normal attention">
+                会場では消毒液の設置を行います。<br>
+                感染状況により、実施内容が変更される可能性があります。
+            </p>
+        </div>
+    </div>
+
+    <div class="section">
+        <h2 class="subtitle">
+            <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/ourresearch.svg" alt="">
+        </h2>
+        <div class="section__content text__center">
+            <p class="section__text font__30">
+                近日公開予定
+            </p>
+        </div>
+    </div>
+
+    <div class="section">
+        <h2 class="subtitle">
+            <img src="http://fundesign.jp/sotsuten2024/wp-content/uploads/2023/11/sns.svg" alt="">
+        </h2>
+        <div class="section__content font__16">
+            <div class="sns">
+                <div class="x section__text">
+                    <i class="fa-brands fa-x-twitter" style="color: #000000; font-size: 40px;"></i>
+                    <a href="http://twitter.com/funsotsuten" target="_blank">エックス</a>
+                </div>
+                <div class="instagram section__text">
+                    <i class="fa-brands fa-instagram" style="color: #000000; font-size: 40px;"></i>
+                    <a href="http://instagram.com/fun_sotsuten" target="_blank">インスタグラム</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </main>
 
 <?php get_footer() ?>
